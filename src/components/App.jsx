acdba26b98de69934149d950761225e9f9450885
Daniel <daniil.spectr@gmail.com>
2023-03-03 15:23:54 +0200

    refactoring app on hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,14 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import Searchbar from './Searchbar';
 import { ImageGallery } from './ImageGallery';
 import '../index.css';
-export class App extends Component {
-  state = {
-    query: '',
-  };
-  handleChange = query => {
-    this.setState({ query });
-  };
-  render() {
-    return (
-      <div className="App">
-        <Searchbar onSubmit={this.handleChange} />
-        <ImageGallery query={this.state.query} />
-      </div> 
-    );
-  }
+export function App() {
+  const [query, setQuery] = useState('');
+
+  return (
+    <div className="App">
+      <Searchbar onSubmit={setQuery} />
+      <ImageGallery query={query} />
+    </div>
+  );
 }
